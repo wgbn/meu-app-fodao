0d5f68bb0feb127457596b0cf2a50740950d329e
Walter Gandarella <walter.wgbn@gmail.com>
2019-03-15 20:01:48 -0300

    meu primeiro commit

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.scss'],
+})
+export class HelloWorldComponent implements OnInit {
+
+  nome: string = 'jusezinho';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
